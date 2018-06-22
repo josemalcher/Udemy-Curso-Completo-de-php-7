5ff14a74cb36468c31ef7890a4fea220e2ab7512
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-06-22 11:59:56 -0300

    098 Classe Page para o Template

--- 24-Projeto-E-Commerce-Admin/index.php
+++ 24-Projeto-E-Commerce-Admin/index.php
@@ -2,16 +2,17 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function () {
 
-    $sql = new Hcode\DB\Sql();
-    $result = $sql->select("select * from tb_users");
-    echo json_encode($result);
+    $page = new Page();
+    $page->setTpl("index");
 
 });
 
